3611798e032b99e4bf76b98dc81061c6662277ce
Zuzanna Surowiec <z.surowiec@millionmonkeyssoftware.com>
2026-01-28 12:27:20 +0100

    nsq

---
+++ lab/pipeline2/rf.r
@@ -0,0 +1,25 @@
+library(TreeDist)
+
+# args: result_tree, publication_tree, output_file, title
+
+args = commandArgs(trailingOnly=TRUE)
+con_tree <- ape::read.tree(args[1])
+tt_tree <- ape::read.tree(args[2])
+
+# con_tree
+#
+# tt_tree
+
+pdf(args[3], width = 12, height = 6)
+VisualizeMatching(
+                  RobinsonFouldsMatching,
+                  con_tree,
+                  tt_tree
+)
+title(main=args[4], sub=sprintf("normalised RF dist: %f", RobinsonFoulds(
+                     con_tree,
+                     tt_tree,
+                     normalize = TRUE
+                     )), line = 3)
+
+
